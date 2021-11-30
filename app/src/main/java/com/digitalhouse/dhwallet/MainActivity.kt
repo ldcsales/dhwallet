b7c79d728beaca93358f3e1d9d905e6aeda8b13a
Lucas Sales <lucas.gol@uol.com.br>
2021-11-29 21:04:46 -0300

    Primeiro Commit

--- app/src/main/java/com/digitalhouse/dhwallet/MainActivity.kt
+++ app/src/main/java/com/digitalhouse/dhwallet/MainActivity.kt
@@ -2,10 +2,19 @@ package com.digitalhouse.dhwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, TransferFragment())
+            .commit()
+
     }
 }
--- app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
+++ app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
@@ -1,4 +1,4 @@
-package com.digitalhouse.dhwallet.util
+package com.digitalhouse.dhwallet
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
